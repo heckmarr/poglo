a860e93f973e60a0bdaf33b074186d622dd1974f
Benjamin Belland <benbelland@gmail.com>
2022-10-16 10:18:17 -0700

    checkpoint, end of example, does not work though

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ fn main() {
 
         //Create a context from an sdl2 window
         #[cfg(feature = "sdl2")]
-        let(gl, shader_version, window, mut events_loop, _context) = {
+        let (gl, shader_version, window, mut events_loop, _context) = {
             let sdl = sdl2::init().unwrap();
             let video = sdl.video().unwrap();
             let gl_attr = video.gl_attr();
@@ -85,10 +85,114 @@ fn main() {
             out vec4 color;
             void main() {
                 color = vec4(vert, 0.5, 1.0);
-            }"#
+            }"#,
         );
 
+        let shader_sources = [
+            (glow::VERTEX_SHADER, vertex_shader_source),
+            (glow::FRAGMENT_SHADER, fragment_shader_source),
+        ];
 
-    };
+        let mut shaders = Vec::with_capacity(shader_sources.len());
+
+        for (shader_type, shader_source) in shader_sources.iter() {
+
+            let shader = gl
+                .create_shader(*shader_type)
+                .expect("Cannot create shader");
+            gl.shader_source(shader, &format!("{}\n{}", shader_version, shader_source));
+            gl.compile_shader(shader);
+            if !gl.get_shader_compile_status(shader) {
+                panic!("{}", gl.get_shader_info_log(shader));
+
+            }
+            gl.attach_shader(program, shader);
+            shaders.push(shader);
+        }
+
+        gl.link_program(program);
+        if !gl.get_program_link_status(program) {
+            panic!("{}", gl.get_program_info_log(program));
+        }
+
+        for shader in shaders {
+            gl.detach_shader(program, shader);
+            gl.delete_shader(shader);
+        }
+
+        gl.use_program(Some(program));
+        gl.clear_color(0.1, 0.2, 0.3, 1.0);
+
+        //Handling events differs between targets
+
+        #[cfg(feature = "glutin")]
+        {
+            use glutin::event::{Event, WindowEvent};
+            use glutin::event_loop::ControlFlow;
+
+            event_loop.run(move |event, _, control_flow| {
+                *control_flow = ControlFlow::Wait;
+                match event {
+                    Event::LoopDestroyed => {
+                        return;
+                    }
+                    Event::MainEventsCleared => {
+                        window.window().request_redraw();
+                    }
+                    Event::RedrawRequested(_) => {
+                        gl.clear(glow::COLOR_BUFFER_BIT);
+                        gl.draw_arrays(glow::TRIANGLES, 0, 3);
+                        window.swap_buffers().unwrap();
+                    }
+                    Event::WindowEvent { ref event, .. } => match event {
+                        WindowEvent::Resized(physical_size) => {
+                            window.resize(*physical_size);
+                        }
+                        WindowEvent::CloseRequested => {
+                            gl.delete_program(program);
+                            gl.delete_vertex_array(vertex_array);
+                            *control_flow = ControlFlow::Exit
+                        }
+                        _ => (),
+                    },
+                    _ => (),
+                }
+            });
+        }
+
+        #[cfg(feature = "sdl2")]
+        {
+            let mut running = true;
+            while running {
+                {
+                for event in events_loop.poll_iter() {
+                    match event {
+                        sdl2::event::Event::Quit { .. } => running = false,
+                        _ => {}
+                    }
+                }
+            }
+
+            gl.clear(glow::COLOR_BUFFER_BIT);
+            gl.draw_arrays(glow::TRIANGLES, 0, 3);
+            window.gl_swap_window();
+
+            if !running {
+                gl.delete_program(program);
+                gl.delete_vertex_array(vertex_array);
+            }
+        }
+    }
+
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        gl.clear(glow::COLOR_BUFFER_BIT);
+        gl.draw_arrays(glow::TRIANGLES, 0, 3);
+        gl.delete_program(program);
+        gl.delete_vertex_array(vertex_array);
+    }
+
+  }
 
 }
