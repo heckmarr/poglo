9852510ef51d57f0ffed0ac6eaff045eb0c0b156
Benjamin Belland <benbelland@gmail.com>
2022-10-16 08:55:55 -0700

    checkpoint three wasm, glutin, sdl

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use glow::*;
 
 fn main() {
     unsafe {
-        //Create a context
+        //Create a context on wasm32 for webGL
         #[cfg(target_arch = "wasm32")]
         let (gl, shader_version) = {
             use wasm_bindgen::JsCast;
@@ -23,6 +23,46 @@ fn main() {
         let gl = glow::Context::from_webgl2_context(webgl2_context);
         (gl, "#version 300 es")
         };
+
+        //Create a context for glutin
+        #[cfg(feature = "glutin")]
+        let (gl, shader_version, window, event_loop) = {
+            let event_loop = glutin::event_loop::EventLoop::new();
+            let window_builder = glutin::window::WindowBuilder::new()
+                .with_title("Hello glutin!")
+                .with_inner_size(glutin::dpi::LogicalSize::new(1024.0, 768.0));
+            let window = glutin::ContextBuilder::new()
+                .with_vsync(true)
+                .build_windowed(window_builder, &event_loop)
+                .unwrap()
+                .make_current()
+                .unwrap();
+            let gl = glow::Context::from_loader_function(|s| window.get_proc_address(s) as *const _);
+            (gl, "#version 410", window, event_loop)
+        };
+
+        //Create a context from an sdl2 window
+        #[cfg(feature = "sdl2")]
+        let(gl, shader_version, window, mut events_loop, _context) = {
+            let sdl = sdl2::init().unwrap();
+            let video = sdl.video().unwrap();
+            let gl_attr = video.gl_attr();
+            gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+            gl_attr.set_context_version(3, 0);
+            let window = video
+                .window("Hallo dorito!", 1024, 769)
+                .opengl()
+                .resizable()
+                .build()
+                .unwrap();
+            let gl_context = window.gl_create_context().unwrap();
+            let gl =
+                glow::Context::from_loader_function(|s| video.gl_get_proc_address(s) as *const _);
+            let event_loop = sdl.event_pump().unwrap();
+            (gl, "#version 130", window, event_loop, gl_context)
+        };
+
+
 
 
     }
