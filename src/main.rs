b0ccf4876346b96b3c5d57079330b4e35792909e
Benjamin Belland <benbelland@gmail.com>
2022-10-17 10:57:24 -0700

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -14,21 +14,14 @@ impl PokeTheBunny {
 
 fn main() {
 
-        let ctx = egui::Context::default();
+        let mut ctx = egui::Context::default();
 
-        loop {
-            let raw_input = egui::RawInput::default();
-            let full_output = ctx.run(raw_input, |ctx| {
-                egui::CentralPanel::default().show(&ctx, |ui|  {
-                    ui.label("Hallo world!");
-                    if ui.button("Click me").clicked() {
-                        println!("Boop");
-                    }
-                });
-            });
- //           handle_platform_output(full_output.platform_output);
-   //         let clipped_primitives = ctx.tessellate(full_output.shapes);
-     //       paint(full_output.textures_delta, clipped_primitives);
-        }
+        let input = egui::RawInput::default();
+        ctx.begin_frame(input);
 
+        egui::CentralPanel::default().show(&ctx, |ui|  {
+            ui.label("Hallo furrkin!");
+        });
+
+        let full_output = ctx.end_frame();
 }
