197d37d295c3a33f633385076703e69a7c4d1422
Benjamin Belland <benbelland@gmail.com>
2022-10-17 12:21:11 -0700

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,17 @@
 use egui;
 
-pub struct PokeTheBunny {
-    furname: String,
-    age: i32,
-}
-
-impl PokeTheBunny {
-    fn ui(&mut self, ui: &mut egui::Ui) {
-        ui.heading("Bunny coding");
-
-    }
-}
 
 fn main() {
 
-        let mut ctx = egui::Context::default();
+        egui::__run_test_ctx(|ctx| {
+                loop {
+                    egui::CentralPanel::default().show(&ctx, |ui|  {
+                    ui.add(egui::Label::new("hola"));
+                    ui.label("Hall000000000000000000000000000000000000 out dere!");
+                    });
+            }
 
-        let input = egui::RawInput::default();
-        ctx.begin_frame(input);
 
-        egui::CentralPanel::default().show(&ctx, |ui|  {
-            ui.label("Hallo furrkin!");
         });
 
-        let full_output = ctx.end_frame();
 }
