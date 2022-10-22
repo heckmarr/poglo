d9c36a299cf9f2d0719fb607ec26b7a9defc9c9e
Benjamin Belland <benbelland@gmail.com>
2022-10-22 13:36:27 -0700

    login modal started

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
-use egui;
+use gtk;
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Builder, Dialog};
 
 
 fn main() {
 
-        egui::__run_test_ctx(|ctx| {
-                loop {
-                    egui::CentralPanel::default().show(&ctx, |ui|  {
-                    ui.add(egui::Label::new("hola"));
-                    ui.label("Hall000000000000000000000000000000000000 out dere!");
-                    });
-            }
+    let application = gtk::Application::new(Some("com.silverpaw.dodecahedral.space"), Default::default(),
+    );
+
+    application.connect_activate(build_ui);
+
+    application.run();
 
 
-        });
+}
+
+
+
+fn build_ui(application: &gtk::Application) {
+        let builder = Builder::from_string(include_str!("../glade/login-modal.ui"));
+
+
+        let window: Dialog = builder.object("login").expect("Couldn't initialize login window.");
+        window.set_application(Some(application));
+
+
+        window.show_all();
+
+
+
 
 }
