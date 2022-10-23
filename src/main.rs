f5cab27f8d56b2082f78e2a2a126744bb2db90d1
Benjamin Belland <benbelland@gmail.com>
2022-10-22 17:19:55 -0700

    gtk styling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use gtk;
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Builder, Dialog};
+use gtk::{Application, ApplicationWindow, Builder, Dialog, StyleContext, CssProvider};
 
 
 fn main() {
@@ -20,9 +20,15 @@ fn main() {
 fn build_ui(application: &gtk::Application) {
         let builder = Builder::from_string(include_str!("../glade/login-modal.ui"));
 
-
         let window: Dialog = builder.object("login").expect("Couldn't initialize login window.");
         window.set_application(Some(application));
+        let style = StyleContext::new();
+        let sheet = CssProvider::new();
+
+        CssProvider::load_from_path(&sheet, "../glade/style.css");
+//        let style = CssProviderExt::load_from_path("../glade/style.css");
+        style.add_provider(&sheet, 1);
+
 
 
         window.show_all();
